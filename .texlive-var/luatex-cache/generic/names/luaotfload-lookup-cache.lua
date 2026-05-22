f325c2a37d9fc22bcdef798c2182555252d2e3c1
Jhonatan S. Blanco <jhonatanblancus@gmail.com>
2026-05-22 12:24:01 -0500

    inclues figures

--- .texlive-var/luatex-cache/generic/names/luaotfload-lookup-cache.lua
+++ .texlive-var/luatex-cache/generic/names/luaotfload-lookup-cache.lua
@@ -1,2 +1,13 @@
 return {
+ ["name:Times New Roman/B:mode=node;script=latn;language=dflt;#b#786433"]={ "/System/Library/Fonts/Supplemental/Times New Roman Bold.ttf", false },
+ ["name:Times New Roman/B:mode=node;script=latn;language=dflt;#b#786434"]={ "/System/Library/Fonts/Supplemental/Times New Roman Bold.ttf", false },
+ ["name:Times New Roman/BI:mode=node;script=latn;language=dflt;#bi#786433"]={ "/System/Library/Fonts/Supplemental/Times New Roman Bold Italic.ttf", false },
+ ["name:Times New Roman/BI:mode=node;script=latn;language=dflt;#bi#786434"]={ "/System/Library/Fonts/Supplemental/Times New Roman Bold Italic.ttf", false },
+ ["name:Times New Roman/I:mode=node;script=latn;language=dflt;#i#786433"]={ "/System/Library/Fonts/Supplemental/Times New Roman Italic.ttf", false },
+ ["name:Times New Roman/I:mode=node;script=latn;language=dflt;#i#786434"]={ "/System/Library/Fonts/Supplemental/Times New Roman Italic.ttf", false },
+ ["name:Times New Roman:mode=node;#786430"]={ "/System/Library/Fonts/Supplemental/Times New Roman.ttf", false },
+ ["name:Times New Roman:mode=node;#786434"]={ "/System/Library/Fonts/Supplemental/Times New Roman.ttf", false },
+ ["name:Times New Roman:mode=node;script=latn;language=dflt;#786432"]={ "/System/Library/Fonts/Supplemental/Times New Roman.ttf", false },
+ ["name:Times New Roman:mode=node;script=latn;language=dflt;#786433"]={ "/System/Library/Fonts/Supplemental/Times New Roman.ttf", false },
+ ["name:Times New Roman:mode=node;script=latn;language=dflt;#786434"]={ "/System/Library/Fonts/Supplemental/Times New Roman.ttf", false },
 }
